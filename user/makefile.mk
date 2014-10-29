1c09547762365f8d3818b1a7e9ff4ee916280f87
cschwalm <schwalm@wisc.edu>
2014-10-28 20:47:34 -0500

    Added Null Test Program

--- user/makefile.mk
+++ user/makefile.mk
@@ -10,6 +10,7 @@ USER_PROGS := \
 	ln\
 	ls\
 	mkdir\
+	nulltest\
 	rm\
 	sh\
 	stressfs\
---
+++ user/nulltest.c
@@ -0,0 +1,17 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main()
+{
+
+	int* n = 0;
+
+	int x = (int) *n;
+
+	printf(1, "The value at pointer NULL is: %d\n", x);
+
+	exit();
+
+	return 0;
+}
